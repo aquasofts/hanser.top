42ce91197cf48f7d3022c71c844f69836a28f6a2
aquasofts <aquasoft520@gmail.com>
2024-10-27 12:11:05 +0800

    1

--- src/config.ts
+++ src/config.ts
@@ -7,8 +7,8 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: '星月落下，编织温柔的童话',
-  subtitle: '无瑕的少女自人群间穿行，将人之美尽收眼底。',
+  title: '毛怪们的图集~',
+  subtitle: '毛怪们的图集~',
   lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -39,34 +39,27 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.Archive,
     LinkPreset.About,
     {
-      name: 'GitHub',
-      url: 'https://github.com/saicaca/fuwari',     // Internal links should not include the base path, as it is automatically added
+      name: 'BiliBIli',
+      url: 'https://space.bilibili.com/11073',     // Internal links should not include the base path, as it is automatically added
       external: true,                               // Show an external link icon and will open in a new tab
     },
   ],
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  avatar: 'assets/images/hanser.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  name: '白色小礼服',
+  bio: 'cece，好快的cece！！',
   links: [
     {
-      name: 'Twitter',
-      icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
-                                        // You will need to install the corresponding icon set if it's not already included
-                                        // `pnpm add @iconify-json/<icon-set-name>`
-      url: 'https://twitter.com',
+      name: 'BiliBIli',
+      icon: 'fa6-brands:bilibili',       // Visit https://icones.js.org/ for icon codes
+      url: 'https://space.bilibili.com/11073',
     },
     {
-      name: 'Steam',
-      icon: 'fa6-brands:steam',
-      url: 'https://store.steampowered.com',
-    },
-    {
-      name: 'GitHub',
-      icon: 'fa6-brands:github',
-      url: 'https://github.com/Aquasofts',
+      name: '虎牙',
+      icon: 'icon-park:tiger-zodiac',
+      url: 'https://www.huya.com/2550505',
     },
   ],
 }
